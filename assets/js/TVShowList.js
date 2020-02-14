dd04db1e228c34d352ded46eaf844b60dc6dd0fc
viri.space <admin@viri.xyz>
2020-02-14 13:36:37 +0000

    improved filtering

--- assets/js/TVShowList.js
+++ assets/js/TVShowList.js
@@ -71,11 +71,11 @@ function sortTable(n) {
   }
 }
 
-// Filter table
+// Filter Title
 function filterTable() {
   // Declare variables
   var input, filter, table, tr, td, i, txtValue;
-  input = document.getElementById("filterInput");
+  input = document.getElementById("filterTitle");
   filter = input.value.toUpperCase();
   table = document.getElementById("TVShowTable");
   tr = table.getElementsByTagName("tr");
@@ -83,6 +83,28 @@ function filterTable() {
   // Loop through all table rows, and hide those who don't match the search query
   for (i = 0; i < tr.length; i++) {
     td = tr[i].getElementsByTagName("td")[0];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
+// Filter Format
+function filterTable() {
+  // Declare variables
+  var input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("filterFormat");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("TVShowTable");
+  tr = table.getElementsByTagName("tr");
+
+  // Loop through all table rows, and hide those who don't match the search query
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[2];
     if (td) {
       txtValue = td.textContent || td.innerText;
       if (txtValue.toUpperCase().indexOf(filter) > -1) {
