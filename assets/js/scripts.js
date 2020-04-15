d0d78f0b3d785fc0d6f9a9a16148af51bc6a666b
viri.space <admin@viri.xyz>
2020-04-15 23:38:40 +0100

    added back button

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,7 @@
 var titles = ['watch your head', 'ok', 'stay hydrated']
 var title = titles[Math.floor(Math.random() * titles.length)] // pick random item
 document.title = title
+
+function goBack() {
+  window.history.back();
+}
