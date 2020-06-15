045ff66bb9be53c6e5e067d2126af939c1441edc
viri.space <admin@viri.xyz>
2020-06-15 18:14:45 +0100

    added getAge function

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,7 +1,18 @@
-var titles = ['watch your head', 'ok', 'stay hydrated']
-var title = titles[Math.floor(Math.random() * titles.length)] // pick random item
-document.title = title
+// var titles = ['watch your head', 'ok', 'stay hydrated']
+// var title = titles[Math.floor(Math.random() * titles.length)] // pick random item
+// document.title = title
+//
+// function goBack() {
+//   window.history.back();
+// }
 
-function goBack() {
-  window.history.back();
+function getAge(dateString) {
+    var today = new Date();
+    var birthDate = new Date(dateString);
+    var age = today.getFullYear() - birthDate.getFullYear();
+    var m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+    }
+    return age;
 }
