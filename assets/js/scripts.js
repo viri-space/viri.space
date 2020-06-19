b603fdbcc1a980a03570321aa8f3a999d752001b
viri.space <admin@viri.xyz>
2020-06-19 02:45:58 +0100

    added random title script back

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,7 +1,7 @@
-// var titles = ['watch your head', 'ok', 'stay hydrated']
-// var title = titles[Math.floor(Math.random() * titles.length)] // pick random item
-// document.title = title
-//
+var titles = ['watch your head', 'ok', 'stay hydrated']
+var title = titles[Math.floor(Math.random() * titles.length)] // pick random item
+document.title = title
+
 // function goBack() {
 //   window.history.back();
 // }
